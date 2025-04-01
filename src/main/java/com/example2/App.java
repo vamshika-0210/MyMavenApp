f19671e3e2fa5eea087c2b31dd77ba407e7c2b05
vamshika-0210 <vamshika0210@gmail.com>
2025-04-01 12:24:56 +0530

    initial commit

---
+++ src/main/java/com/example2/App.java
@@ -0,0 +1,15 @@
+package com.example2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int a=10;
+        int b=10;
+        System.out.println("sum:"+(a+b));
+    }
+}
